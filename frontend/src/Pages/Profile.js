8d1966829fedaf9b152808be3f353aa055706695
عبدالله الصبي <Abdullahb.alsabi@gmail.com>
2021-11-14 15:31:35 +0300

    update profile

--- frontend/src/Pages/Profile.js
+++ frontend/src/Pages/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Pages.css";
 function Profile() {
   let date = new Date();
@@ -6,13 +6,36 @@ function Profile() {
   let day = date.getDate();
   let year = date.getFullYear();
   let newdate = year + "/" + month + "/" + day;
+  function hundleEdit() {
+    let elements = document.getElementsByTagName("input");
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].disabled = false;
+    }
+  }
+  function hundleUpdate() {
+    let elements = document.getElementsByTagName("input");
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].disabled = true;
+    }
+  }
 
   return (
     <div className=" min-h-full flex flex-col p-2 pt-4 md:p-10 lg:p-10 xl:p-10">
       <div className="container  max-w-lg m-auto  flex flex-1 flex-col items-center justify-center px-20 ">
         <div className="bg-white flex flex-col px-6 py-8 rounded shadow-md text-black cardWidth  ">
-          <h1 className="mb-8 text-3xl text-center">User Profile</h1>
+          <div className="flex flex-row justify-between">
+            <h1 className="mb-8 text-3xl text-center">User Profile</h1>
+            <button
+              onClick={hundleEdit}
+              type="submit"
+              className="w-4/12 text-center py-3 rounded bg-green-500 text-white hover:bg-green-700  focus:outline-none my-1"
+            >
+              {" "}
+              Edit
+            </button>
+          </div>
           <hr className="pb-5" />
+
           <input
             type="text"
             className="block border border-gray-400 w-full p-3 rounded mb-4"
@@ -46,6 +69,7 @@ function Profile() {
             className="block border border-gray-400 w-full p-3 rounded mb-4"
             name="height"
             placeholder={"180" + " cm"}
+            disabled
           />
           <input
             type="text"
@@ -54,6 +78,13 @@ function Profile() {
             placeholder={"80" + " kg"}
             disabled
           />
+          <button
+            onClick={hundleUpdate}
+            type="submit"
+            className="w-auto text-center py-3 rounded bg-green-500 text-white hover:bg-green-700  focus:outline-none my-1"
+          >
+            Update
+          </button>
         </div>
       </div>
     </div>
