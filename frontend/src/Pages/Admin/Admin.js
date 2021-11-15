a482996b6604f80499fa35348345fad48bf43116
Abdulmajeed Alduaifi <438101329@student.ksu.edu.sa>
2021-11-15 12:51:08 +0300

    change import

--- frontend/src/Pages/Admin/Admin.js
+++ frontend/src/Pages/Admin/Admin.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import p1 from ".././../images/p1.jpg";
+
 import axios from "axios";
 function Admin() {
   const [programs, setPrograms] = useState([]);
